171d6588b9479d7ce3b0f61b8aa8d22ec0a0e0d5
Ohgone <107023268+Ohgone@users.noreply.github.com>
2023-07-23 17:12:59 +0900

    Create CDLL.h

---
+++ C/자료구조+알고리즘/1. List/CDLL/CDLL.h
@@ -0,0 +1,23 @@
+#ifndef CDLL
+#define CDLL
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct tagNode{
+	ElementType Data;
+	struct tagNode* PrevNode;
+	struct tagNode* NextNode;
+} Node;
+
+Node* CDLL_CreateNode(ElementType NewData);
+void CDLL_DestroyNode(Node* Node);
+void CDLL_AppendNode(Node** Head, Node* NewNode);
+void CDLL_InsertAfter(Node* Current, Node* NewNode);
+void CDLL_RemoveNode(Node** Head, Node* Remove);
+Node* CDLL_GetNodeAt(Node* Head, int Location);
+int CDLL_GetNodeCount(Node* Head);
+
+#endif
